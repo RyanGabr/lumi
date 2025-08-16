72f6f699e79dfad9740c56ed7bf4a150ad410a07
RyanGabr <ryanalexandregabriel@gmail.com>
2025-08-16 15:52:15 -0300

    feat(gallery): add empty state UI when no categories exist

--- src/components/gallery/categories-list.tsx
+++ src/components/gallery/categories-list.tsx
@@ -5,6 +5,24 @@ import { useGetCategories } from "@/hooks/use-category";
 export function CategoriesList() {
   const { data: categories } = useGetCategories();
 
+  if (!categories || categories.length === 0) {
+    return (
+      <div className="space-y-4 select-none">
+        <span className="flex items-center gap-1.5 text-xs font-bold text-foreground/50">
+          <Squares2X2Icon className="size-3.5" />
+          Categorias
+        </span>
+        <div className="bg-foreground/5 w-full h-80 rounded-3xl flex flex-col items-center justify-center gap-2">
+          <Squares2X2Icon className="size-12 fill-foreground/50" />
+          <h3 className="font-medium">
+            Você ainda não possui nenhuma categoria
+          </h3>
+          <p className="text-sm text-foreground/50">Crie sua primeira categoria</p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="space-y-4 select-none">
       <span className="flex items-center gap-1.5 text-xs font-bold text-foreground/50">
@@ -13,10 +31,7 @@ export function CategoriesList() {
       </span>
       <div className="grid grid-cols-5 gap-5 items-center">
         {categories?.map((category, index) => (
-          <CategoryCard
-            key={index}
-            category_name={category.name}
-          />
+          <CategoryCard key={index} category_name={category.name} />
         ))}
       </div>
     </div>
