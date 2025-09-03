a423040dfb4ae016f985d010900fc679ba5480ae
RyanGabr <ryanalexandregabriel@gmail.com>
2025-09-03 15:35:38 -0300

    style(gallery): improve suspense loading UI

--- src/pages/app/gallery.tsx
+++ src/pages/app/gallery.tsx
@@ -1,20 +1,17 @@
 import { CategoriesList } from "@/components/gallery/categories-list";
 import { GalleryHero } from "@/components/gallery/hero";
 import { ImagesList } from "@/components/gallery/images-list";
-import { LoadingCategoriesList } from "@/components/gallery/loading-categories-list";
-import { LoadingImagesList } from "@/components/gallery/loading-images-list";
+import { LoadingGallery } from "@/components/gallery/loading-gallery";
 import { Suspense } from "react";
 
 export function Gallery() {
   return (
-    <div className="space-y-7 xl:w-10/12 2xl:w-8/12 mx-auto p-5 md:p-7 md:px-10">
-      <GalleryHero />
-      <Suspense fallback={<LoadingCategoriesList />}>
+    <Suspense fallback={<LoadingGallery />}>
+      <div className="space-y-7 xl:w-10/12 2xl:w-8/12 mx-auto p-5 md:p-7 md:px-10">
+        <GalleryHero />
         <CategoriesList />
-      </Suspense>
-      <Suspense fallback={<LoadingImagesList />}>
         <ImagesList />
-      </Suspense>
-    </div>
+      </div>
+    </Suspense>
   );
 }
