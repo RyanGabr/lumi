ffd28ef2bf2588c019e5822b3804117479eaf50d
RyanGabr <ryanalexandregabriel@gmail.com>
2025-09-07 19:21:03 -0300

    feat(gallery): add custom document title

--- src/pages/app/gallery.tsx
+++ src/pages/app/gallery.tsx
@@ -2,9 +2,13 @@ import { CategoriesList } from "@/components/gallery/categories-list";
 import { GalleryHero } from "@/components/gallery/hero";
 import { ImagesList } from "@/components/gallery/images-list";
 import { LoadingGallery } from "@/components/gallery/loading-gallery";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 
 export function Gallery() {
+  useEffect(() => {
+    document.title = "Lumi | Página inicial";
+  }, []);
+
   return (
     <Suspense fallback={<LoadingGallery />}>
       <div className="space-y-7 w-full xl:w-10/12 2xl:w-8/12 mx-auto p-5 md:p-7 md:px-10">
