631aa5baf1c4153f9ac471c754eb1dfc342b2929
RyanGabr <ryanalexandregabriel@gmail.com>
2025-09-07 11:24:44 -0300

    style(gallery): add hover effect to CategoryCard

--- src/components/gallery/category-card.tsx
+++ src/components/gallery/category-card.tsx
@@ -9,13 +9,15 @@ export function CategoryCard({ category }: CategoryCardProps) {
   return (
     <Link
       to={`/category?${category.id}`}
-      className="w-[1fr] h-44 rounded-3xl bg-foreground/5 flex flex-col justify-between transition-all hover:bg-foreground/7 select-none"
+      className="w-[1fr] h-40 rounded-3xl dark:bg-foreground/5 flex flex-col justify-between transition-all dark:hover:bg-foreground/7 hover:bg-foreground/7 select-none"
     >
       <div className="p-4 bg-foreground/7 rounded-t-3xl">
         <div data-color={category.color} className="size-3 rounded-full" />
       </div>
       <div className="p-5">
-        <span className="font-medium text-foreground/60 text-sm">{category.name}</span>
+        <h3 className="font-medium text-foreground/60 text-sm leading-5">
+          {category.name}
+        </h3>
       </div>
     </Link>
   );
