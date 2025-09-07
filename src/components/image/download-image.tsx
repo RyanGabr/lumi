d491dbda8de8d1f4fda5ecd8b92bf75aca15a456
RyanGabr <ryanalexandregabriel@gmail.com>
2025-09-07 13:23:50 -0300

    feat(image): add download image option to context menu

---
+++ src/components/image/download-image.tsx
@@ -0,0 +1,51 @@
+import { Download } from "lucide-react";
+import { ContextMenuItem } from "../ui/context-menu";
+import { supabase } from "@/lib/supabase";
+import type { ImageType } from "@/types/image";
+
+interface DownloadImageProps {
+  image: ImageType;
+}
+
+export function DownloadImage({ image }: DownloadImageProps) {
+  async function downloadImage() {
+    const { data } = supabase.storage.from("images").getPublicUrl(image.path);
+
+    const publicUrl: string = data?.publicUrl ?? "";
+    if (!publicUrl) {
+      console.error("Public URL não encontrada");
+      return;
+    }
+
+    try {
+      const response = await fetch(publicUrl);
+      if (!response.ok) {
+        throw new Error(`Erro ao buscar a imagem: ${response.statusText}`);
+      }
+
+      const blob: Blob = await response.blob();
+      const url: string = URL.createObjectURL(blob);
+
+      const link: HTMLAnchorElement = document.createElement("a");
+      link.href = url;
+
+      const fileName: string = image.path.split("/").pop() ?? "download.png";
+      link.download = fileName;
+
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+
+      URL.revokeObjectURL(url);
+    } catch (err) {
+      console.error("Erro ao baixar a imagem:", err);
+    }
+  }
+
+  return (
+    <ContextMenuItem onClick={downloadImage}>
+      <Download />
+      Fazer download
+    </ContextMenuItem>
+  );
+}
