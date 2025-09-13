4025e07917b4fd25b9b548a7b07808f15186d0c8
RyanGabr <ryanalexandregabriel@gmail.com>
2025-09-13 16:32:44 -0300

    refactor(image): use local context to avoid prop drilling

--- src/components/image/image.tsx
+++ src/components/image/image.tsx
@@ -5,39 +5,51 @@ import type { ImageType } from "@/types/image";
 import { ImageContextMenu } from "./image-context-menu";
 import { Dialog, DialogTrigger } from "../ui/dialog";
 import { ImageDetails } from "./image-details";
+import { createContext, useContext } from "react";
 
 interface ImageProps {
   image: ImageType;
 }
 
+const ImageContext = createContext<ImageType | null>(null);
+
+export function useImage() {
+  const context = useContext(ImageContext);
+  if (!context) throw new Error("useImage must be used within <Image>");
+  return context;
+}
+
 export function Image({ image }: ImageProps) {
   return (
-    <Dialog>
-      <ContextMenu>
-        <DialogTrigger asChild>
-          <ContextMenuTrigger asChild>
-            <button type="button" className="relative h-72 cursor-pointer">
-              {image.is_favorite && (
-                <Tooltip>
-                  <TooltipTrigger className="absolute right-4 top-4 w-fit h-fit z-50">
-                    <Star className="size-5 fill-yellow-400 text-transparent" />
-                  </TooltipTrigger>
-                  <TooltipContent>Imagem favoritada</TooltipContent>
-                </Tooltip>
-              )}
-              <img
-                key={image.id}
-                src={image.image_url}
-                className="w-full h-72 rounded-xl object-cover transition-all hover:brightness-85 border-2 border-border/50"
-              />
-            </button>
-          </ContextMenuTrigger>
-        </DialogTrigger>
+    <ImageContext.Provider value={image}>
+      <Dialog>
+        <ContextMenu>
+          <DialogTrigger asChild>
+            <ContextMenuTrigger asChild>
+              <button type="button" className="relative h-72 cursor-pointer">
+                {image.is_favorite && (
+                  <Tooltip>
+                    <TooltipTrigger className="absolute right-4 top-4 w-fit h-fit z-50">
+                      <Star className="size-5 fill-yellow-400 text-transparent" />
+                    </TooltipTrigger>
+                    <TooltipContent>Imagem favoritada</TooltipContent>
+                  </Tooltip>
+                )}
+                <img
+                  key={image.id}
+                  src={image.image_url}
+                  className="w-full h-72 rounded-xl object-cover transition-all hover:brightness-85 border-2 border-border/50"
+                />
+              </button>
+            </ContextMenuTrigger>
+          </DialogTrigger>
 
-        <ImageContextMenu image={image} />
-      </ContextMenu>
-
-      <ImageDetails image={image} />
-    </Dialog>
+          <ImageContextMenu />
+        </ContextMenu>
+        
+        {/* Image Details => Dialog Content */}
+        <ImageDetails />
+      </Dialog>
+    </ImageContext.Provider>
   );
 }
