6a947d7e62b0c68077a3964c48a163f079cfdf6f
RyanGabr <ryanalexandregabriel@gmail.com>
2025-09-15 09:43:26 -0300

    style(category): improve header UI

--- src/components/category/header.tsx
+++ src/components/category/header.tsx
@@ -1,8 +1,7 @@
 import { useGetCategoryById } from "@/hooks/use-get-categories";
-import { Squares2X2Icon } from "@heroicons/react/16/solid";
 import { SlashIcon } from "@heroicons/react/20/solid";
 import { useLocation } from "react-router-dom";
-import { DeleteDialog } from "./delete-dialog";
+import { DeleteDialog } from "./delete-category";
 import { CreateImage } from "./create-image";
 import { EditCategory } from "./edit-category";
 
@@ -21,25 +20,38 @@ export function Header() {
   }).format(date);
 
   return (
-    <div className="w-full flex items-center justify-between py-2 px-5">
-      <div className="flex items-center gap-0.5 cursor-default select-none">
-        <div className="flex items-center gap-1.5">
-          <Squares2X2Icon className="size-4 fill-foreground/50" />
+    <div className="flex flex-col">
+      <header className="w-full flex items-center justify-between py-2 px-5">
+        {/* Left side */}
+        <div className="flex items-center gap-0.5 cursor-default select-none">
           <span className="font-medium text-sm">Categorias</span>
+          <SlashIcon className="size-5 fill-foreground/20" />
+          <div className="flex items-center gap-2">
+            <span className="font-semibold text-sm truncate text-ellipsis">
+              {category?.name}
+            </span>
+          </div>
         </div>
-        <SlashIcon className="size-5 fill-foreground/20" />
-        <div className="flex items-center gap-2">
-          <div data-color={category?.color} className="size-2 rounded-full" />
-          <span className="font-medium text-sm">{category?.name}</span>
+
+        {/* Right side */}
+        <div className="flex items-center">
+          <span className="font-medium text-sm text-foreground/40 mr-2 select-none hidden lg:block">
+            Criada em {created_at_date}
+          </span>
+          <CreateImage />
+          <EditCategory />
+          <DeleteDialog />
         </div>
-      </div>
-      <div className="flex items-center">
-        <span className="font-medium text-sm text-foreground/40 mr-2 select-none">
-          Criada em {created_at_date}
-        </span>
-        <CreateImage />
-        <EditCategory />
-        <DeleteDialog />
+      </header>
+
+      {/* Banner */}
+      <div className="relative">
+        <div className="h-32 w-full bg-foreground/10" />
+        <div className="w-full xl:w-10/12 2xl:w-8/12 mx-auto px-5 md:px-10">
+          <div className="size-16 bg-background rounded-full border-2 border-border flex items-center justify-center absolute -bottom-10">
+            <div data-color={category.color} className="size-6 rounded-full" />
+          </div>
+        </div>
       </div>
     </div>
   );
