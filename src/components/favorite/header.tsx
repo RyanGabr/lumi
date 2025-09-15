a88e62282fa1da5f913df2a15a4f8f28bf3dacef
RyanGabr <ryanalexandregabriel@gmail.com>
2025-09-15 09:49:40 -0300

    style(favorite): improve header UI

--- src/components/favorite/header.tsx
+++ src/components/favorite/header.tsx
@@ -1,17 +1,29 @@
-import { StarIcon } from "@heroicons/react/16/solid";
 import { Questions } from "./questions";
+import { Star } from "lucide-react";
 
 export function Header() {
   return (
-    <div className="w-full flex items-center justify-between py-2 px-5">
-      <div className="cursor-default select-none">
-        <div className="flex items-center gap-1.5">
-          <StarIcon className="size-4.5 fill-yellow-400"/>
-          <span className="font-medium text-sm">Favoritos</span>
+    <div className="flex flex-col">
+      <header className="w-full flex items-center justify-between py-2 px-5">
+        {/* Left side */}
+        <div className="cursor-default select-none">
+          <span className="font-semibold text-sm">Favoritos</span>
         </div>
-      </div>
-      <div className="flex items-center">
-        <Questions />
+
+        {/* Right side */}
+        <div className="flex items-center">
+          <Questions />
+        </div>
+      </header>
+
+      {/* Banner */}
+      <div className="relative">
+        <div className="h-32 w-full bg-foreground/10" />
+        <div className="w-full xl:w-10/12 2xl:w-8/12 mx-auto px-5 md:px-10">
+          <div className="size-16 bg-background rounded-xl border-2 border-border flex items-center justify-center absolute -bottom-10">
+            <Star className="size-8 text-transparent fill-yellow-400" />
+          </div>
+        </div>
       </div>
     </div>
   );
