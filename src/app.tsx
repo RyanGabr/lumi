5f25c576e4779d97b1748b8f5ce15499e25dc278
RyanGabr <ryanalexandregabriel@gmail.com>
2025-10-21 09:58:58 -0300

    fix: correct the file name to app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,21 @@
+import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "./components/ui/theme-provider";
+import { routes } from "./routes/routes";
+import { supabase } from "./lib/supabase";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/query-client";
+import { Toaster } from "./components/ui/sonner";
+
+export function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <SessionContextProvider supabaseClient={supabase}>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={routes} />
+          <Toaster />
+        </QueryClientProvider>
+      </SessionContextProvider>
+    </ThemeProvider>
+  );
+}
